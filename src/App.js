85883efd21591860980dccb0ca47459e09e7cedd
19mddilAspire <mddilshadul98@gmail.com>
2022-11-13 16:18:31 +0600

    jsx and react library

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
+import React from 'react';
 import './App.css';
 
+//JSX
 function App() {
   return (
     <div className="App">
       <h1>Those who strive in our way, we will guide them.</h1>
+      <Person />
     </div>
   );
+}
+
+function Person() {
+  return (
+    <div>
+      <h1>This is a strange person.</h1>
+    </div>
+  )
 }
 
 export default App;
