24c03d1a7da5b0b895424438a022d61abc48aff9
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-11-16 01:57:32 +0600

    basic syntax and styling on project

--- src/App.js
+++ src/App.js
@@ -9,17 +9,55 @@ class App extends Component {
       { bookName: "Alchemy of Happiness", writer: "Imam Gazzali" },
       { bookName: "The Lean StartUp", writer: "Eric Ries" },
       { bookName: "The Godfather", writer: "Mark Paulo" }
-    ]
+    ],
+    someOtherProps: "And I love her",
+  }
+
+  changeBookState = (bookWriter) => {
+    this.setState({
+      book: [
+        { bookName: "Alchemy of Happiness", writer: "Imam Gazzali" },
+        { bookName: "The Lean StartUp", writer: "Eric Ries" },
+        { bookName: "The Godfather", writer: bookWriter }
+      ]
+    })
+  }
+
+  changeWithInputState = event => {
+    this.setState({
+      book: [
+        { bookName: "Alchemy of Happiness", writer: "Imam Gazzali" },
+        { bookName: "The Lean StartUp", writer: "Eric Ries" },
+        { bookName: "The Godfather", writer: event.target.value }
+      ]
+    })
   }
   render() {
-    // let obj = new Component();
-    // console.log(obj);
+    const style = {
+      color: "grey",
+      backgroundColor: "black",
+      marginLeft: "40%",
+      marginRight: "40%",
+      padding: "5px",
+    }
     return (
       <div className="App">
-        <h1>BookList</h1>
-        <Book bookName={this.state.book[0].bookName} writer={this.state.book[0].writer} />
-        <Book bookName={this.state.book[1].bookName} writer={this.state.book[1].writer} />
-        <Book bookName={this.state.book[2].bookName} writer={this.state.book[2].writer} />
+        <h1 style={style}>BOOKLIST</h1>
+
+        <button onClick={this.changeBookState.bind(this, "MARIO PAULO")}>Change State</button>
+        <input type="text" onChange={this.changeWithInputState} />
+        <Book
+          bookName={this.state.book[0].bookName}
+          writer={this.state.book[0].writer} />
+        <Book
+          bookName={this.state.book[1].bookName}
+          writer={this.state.book[1].writer} />
+        <Book
+          bookName={this.state.book[2].bookName}
+          writer={this.state.book[2].writer}
+          change={this.changeBookState.bind(this, "mario paulo")}
+          hide='false'
+          inputName={this.changeWithInputState} />
       </div>
     );
   }
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../stylesheets/Book.css'
 
 const Book = props => (
-    <div>
-        <h2>{props.bookName}</h2>
-        <h4>{props.writer}</h4>
+    <div className='Book'>
+        <h2 >{props.bookName}</h2>
+        <h4 onClick={props.change} >{props.writer}</h4>
+        <input hidden={!(props.hide === 'false')} type="text" onChange={props.inputName} value={props.writer} />
     </div>
 )
 
