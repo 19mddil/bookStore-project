9803ef113fa6fa587068dc5ee0ff4e8e96eaeaec
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-11-17 15:48:18 +0600

    added delete property

--- src/App.js
+++ src/App.js
@@ -1,66 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 import Book from './components/Book';
+import './stylesheets/Book.css'
 
 //JSX
 class App extends Component {
   state = {
-    book: [
+    books: [
       { bookName: "Alchemy of Happiness", writer: "Imam Gazzali" },
       { bookName: "The Lean StartUp", writer: "Eric Ries" },
       { bookName: "The Godfather", writer: "Mark Paulo" }
     ],
-    someOtherProps: "And I love her",
   }
 
-  changeBookState = (bookWriter) => {
+  deleteBook = index => {
+    const books = [...this.state.books];
+    books.splice(index, 1);
     this.setState({
-      book: [
-        { bookName: "Alchemy of Happiness", writer: "Imam Gazzali" },
-        { bookName: "The Lean StartUp", writer: "Eric Ries" },
-        { bookName: "The Godfather", writer: bookWriter }
-      ]
-    })
+      books: books,
+    });
   }
 
-  changeWithInputState = event => {
-    this.setState({
-      book: [
-        { bookName: "Alchemy of Happiness", writer: "Imam Gazzali" },
-        { bookName: "The Lean StartUp", writer: "Eric Ries" },
-        { bookName: "The Godfather", writer: event.target.value }
-      ]
-    })
-  }
   render() {
-    const style = {
-      color: "grey",
-      backgroundColor: "black",
-      marginLeft: "40%",
-      marginRight: "40%",
-      padding: "5px",
-    }
     return (
       <div className="App">
-        <h1 style={style}>BOOKLIST</h1>
+        <h1 style={{
+          color: "grey",
+          backgroundColor: "black",
+          marginLeft: "40%",
+          marginRight: "40%",
+          padding: "5px",
+        }}>BOOKLIST</h1>
 
-        <button onClick={this.changeBookState.bind(this, "MARIO PAULO")}>Change State</button>
-        <input type="text" onChange={this.changeWithInputState} />
-        <Book
-          bookName={this.state.book[0].bookName}
-          writer={this.state.book[0].writer} />
-        <Book
-          bookName={this.state.book[1].bookName}
-          writer={this.state.book[1].writer} />
-        <Book
-          bookName={this.state.book[2].bookName}
-          writer={this.state.book[2].writer}
-          change={this.changeBookState.bind(this, "mario paulo")}
-          hide='false'
-          inputName={this.changeWithInputState} />
+        {
+          this.state.books.map((book, index) => {
+            return (
+              <Book
+                bookName={book.bookName}
+                writer={book.writer}
+                delete={this.deleteBook.bind(this, index)}
+              />
+            )
+          })
+        }
       </div>
     );
   }
+
 }
 
 export default App;
--- src/components/Book.js
+++ src/components/Book.js
@@ -3,9 +3,8 @@ import '../stylesheets/Book.css'
 
 const Book = props => (
     <div className='Book'>
-        <h2 >{props.bookName}</h2>
-        <h4 onClick={props.change} >{props.writer}</h4>
-        <input hidden={!(props.hide === 'false')} type="text" onChange={props.inputName} value={props.writer} />
+        <h2 onClick={props.delete} >{props.bookName}</h2>
+        <h4 >{props.writer}</h4>
     </div>
 )
 
