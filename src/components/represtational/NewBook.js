68706cdc634be91a3b6be43688d69dd64d6df253
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-12-02 23:53:33 +0600

    connected a form to state

--- src/components/represtational/NewBook.js
+++ src/components/represtational/NewBook.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NewBook = props => {
-    return (
-        <div>
-            <h1>New Book entry</h1>
-        </div>
-    )
+class NewBook extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bookName: null,
+            writer: null,
+            description: null,
+        }
+    }
+
+    textHandler = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+    render() {
+        return (
+            <div>
+                <h1>New Book entry</h1>
+                <form>
+                    <label>Book Name:</label>
+                    <br />
+                    <input type="text" name="bookName" value={this.state.bookName} onChange={(event) => this.textHandler(event)} />
+                    <br />
+                    <label>Writer Name:</label>
+                    <br />
+                    <input type="text" name="writer" value={this.state.writer} onChange={(event) => this.textHandler(event)} />
+                    <br />
+                    <label>Description:</label>
+                    <br />
+                    <textarea name="description" value={this.state.description} onChange={(event) => this.textHandler(event)} />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default NewBook;
